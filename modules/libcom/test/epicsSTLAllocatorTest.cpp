e950456c1b8fadf7ab120e601f1625d0cf99b723
Martin Konrad <konrad@frib.msu.edu>
2019-08-06 15:45:20 -0400

    Destroy allocator before printing test report

--- modules/libcom/test/epicsSTLAllocatorTest.cpp
+++ modules/libcom/test/epicsSTLAllocatorTest.cpp
@@ -12,22 +12,24 @@ MAIN(stlAllocatorTest)
 {
     testPlan(11);
 
-    testDiag("Allocating a std::vector using epicsSTLAllocator backed by epicsMallocator");
+    {
+      testDiag("Allocating a std::vector using epicsSTLAllocator backed by epicsMallocator");
 
-    typedef stl<int, mallocator> stlAllocator;
+      typedef stl<int, mallocator> stlAllocator;
 
-    const int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    size_t arraysize = sizeof(numbers) / sizeof(numbers[0]);
+      const int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+      size_t arraysize = sizeof(numbers) / sizeof(numbers[0]);
 
-    std::vector<int, stlAllocator> v;
-    for (size_t i = 0; i < arraysize; ++i) {
-        v.push_back(numbers[i]);
-    }
+      std::vector<int, stlAllocator> v;
+      for (size_t i = 0; i < arraysize; ++i) {
+          v.push_back(numbers[i]);
+      }
 
-    testOk(v.size() == arraysize, "vector size %zu, number of pushed elements %zu", v.size(), arraysize);
+      testOk(v.size() == arraysize, "vector size %zu, number of pushed elements %zu", v.size(), arraysize);
 
-    for (size_t i = 0; i < v.size(); ++i) {
-        testOk(v[i] == numbers[i], "compare vector element %zu: pushed %d, read back: %d", i, numbers[i], v[i]);
+      for (size_t i = 0; i < v.size(); ++i) {
+          testOk(v[i] == numbers[i], "compare vector element %zu: pushed %d, read back: %d", i, numbers[i], v[i]);
+      }
     }
 
     return testDone();
