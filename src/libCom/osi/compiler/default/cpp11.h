9d9c8c04a634c0dd0a433484896d62ea995e2924
Martin Konrad <konrad@frib.msu.edu>
2020-02-21 08:02:29 -0500

    Implement for compilers other than GCC and clang

---
+++ src/libCom/osi/compiler/default/cpp11.h
@@ -0,0 +1,15 @@
+#ifndef cpp11_h
+#define cpp11_h
+ 
+#if __cplusplus >= 201103L
+// https://en.cppreference.com/w/cpp/language/noexcept
+#  define noexcept
+
+// https://en.cppreference.com/w/cpp/language/final
+#  define final
+
+// https://en.cppreference.com/w/cpp/language/override
+#  define override
+#endif
+
+#endif /* cpp11_h */
