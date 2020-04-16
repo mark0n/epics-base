70dd9d216c50536656c0fae466d54aa2ea7d7c1a
Jeffrey Owen Hill <johill@lanl.gov>
2020-04-15 22:07:55 -0600

    added banner

--- modules/libcom/test/epicsStaticInstanceTest.cpp
+++ modules/libcom/test/epicsStaticInstanceTest.cpp
@@ -1,3 +1,14 @@
+/*************************************************************************\
+* Copyright (c) 2020 Triad National Security, as operator of Los Alamos 
+*     National Laboratory
+* EPICS BASE is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution.
+\*************************************************************************/
+/*
+ *      Author  Jeffrey O. Hill
+ *              johill@lanl.gov
+ *              505 665 1831
+ */
 
 #include <cstdlib>
 
